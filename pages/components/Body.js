6b943d3e555a94bad94de03c6e47d2f8836b2222
Onwara <117433911+WARA-ctrl@users.noreply.github.com>
2022-12-12 00:15:47 +0700

    Update Body.js

--- pages/components/Body.js
+++ pages/components/Body.js
@@ -1,3 +1,4 @@
+
 import React, { useContext } from "react";
 import { AppContext } from "..";
 // import axios from "axios";
@@ -20,34 +21,22 @@ const Body = () => {
   const [dataFilter, setDataFilter] = useState(["name"]);
   // const [shop, setShop] = useState([]);
   const [shopData, setShopData] = useState([]);
-  const useAble = (shop) => {
+  const xAble = (shop) => {
     setShopData(shop.features);
+    console.log("is shop data", shopData);
     // setDataFilter(shop.features[i].properties.name);
   };
 
   useEffect(() => {
     fetch(
-      "https://api.geoapify.com/v2/places?categories=catering.restaurant&bias=proximity:100.523186,13.736717&limit=450&apiKey=f85e677b429c4c0e92ce5b005c3642b1"
+      `https://api.geoapify.com/v2/places?categories=catering.restaurant&bias=proximity:100.523186,13.736717&limit=20&name=${word}&apiKey=f85e677b429c4c0e92ce5b005c3642b1`
     )
       .then((response) => response.json())
-      .then((result) => useAble(result))
-      .catch((error) => console.log("error", error));
-    console.log(shopData);
+      .then((result) => xAble(result))
+      .catch((error) => setShopData([]));
     // console.log(shop.features[0].properties.name);
-  }, []);
-
-  // const searchShop = (shops) => {
-  //   return shops.filter((item) => {
-  //     return dataFilter.some((filter) => {
-  //       if (item[filter]) {
-  //         return (
-  //           item[filter].toString().toLowerCase().indexOf(word.toLowerCase()) >
-  //           -1
-  //         );
-  //       }
-  //     });
-  //   });
-  // };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [word]);
 
   const searchShop = (shops) => {
     return shops.filter((item) => {
@@ -65,21 +54,26 @@ const Body = () => {
   return (
     <div className="shop">
       <ul>
-        {searchShop(shopData).map((item, index) => {
-          return (
-            <li key={index} className="card">
-              <h1>{item.properties.name}</h1>
-              <h3>
-                {item.properties.address_line1}
-                {item.properties.address_line2}
-              </h3>
-              <h4>Distance : {item.properties.distance} km</h4>
-            </li>
-          );
-        })}
+        {/* {shopData && <div>{shopData.length}</div>} */}
+        {!shopData || (shopData.length === 0 && <div>no data</div>)}
+        {shopData &&
+          shopData.length > 0 &&
+          shopData.map((item, index) => {
+            return (
+              <li key={index} className="card">
+                <h1>{item.properties.name}</h1>
+                <h3>
+                  {item.properties.address_line1}
+                  {item.properties.address_line2}
+                </h3>
+                <h4>Distance : {item.properties.distance} m</h4>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
 };
 
 export default Body;
+
